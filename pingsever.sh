8d9c8d319a15dd9ee0065ee8f8eb8c29e6bd57af
spookyMC123 <editormaster505@gmail.com>
2025-07-17 15:11:51 +0600

    Create pingsever.sh

---
+++ pingsever.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Made by Joy
+#clear
+clear
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo -e "${RED}Please run this script as root.${NC}"
+  exit 1
+fi
+
+echo "                            power by InfinityForge                                           "
+
+echo "* Installing Dependencies"
+sudo apt update
+sudo apt upgrade -y
+sudo apt install nodejs npm -y
+sudo apt update
+
+echo "📦 Installing 24/7 server..."
+
+# Setup directories
+mkdir -p 24/7/sever
+cd 24/7/sever || exit
+
+# Write docker-compose.yml
+cat <<'EOF' > server.js
+// server.js
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("✅ Codespace is alive! " + new Date());
+});
+
+server.listen(8089, () => {
+  console.log("✅ HTTP server running on port 8089");
+});
+
+EOF
+
+# run server
+node server.js
+
+while true; do
+  curl -s http://localhost:8089 > /dev/null
+  echo "🟢 Self ping: $(date)"
+  sleep 240
+done
+
+
+echo "✅ server is now running!"
+echo "🌐 copy it :go to port and copy http://localhost:8089 or your external IP on port 8089"
